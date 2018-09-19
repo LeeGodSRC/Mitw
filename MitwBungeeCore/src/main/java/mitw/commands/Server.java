5ced087f8cee2c248c38f1005016d2092b1fc794
lulu2002 <lose9749@gmail.com>
2018-09-19 20:00:35 +0800

    add permission to mBungee /server cmd

--- MitwBungeeCore/src/main/java/mitw/commands/Server.java
+++ MitwBungeeCore/src/main/java/mitw/commands/Server.java
@@ -13,7 +13,7 @@ import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
-public class Server extends Command{
+public class Server extends Command {
 
 	public Server(Bungee main) {
 		super("server");
@@ -23,20 +23,24 @@ public class Server extends Command{
 	@Override
 	public void execute(CommandSender sender, String[] args) {
 
-		final ProxiedPlayer player = (ProxiedPlayer) sender;
-		if(args.length == 0){
-			player.sendMessage(Bungee.Prefix + ChatColor.GRAY + "你現在正處於 " + ChatColor.GOLD + player.getServer().getInfo().getName() + ChatColor.GRAY + " 伺服器");
+		if (!sender.hasPermission("mitw.admin"))
 			return;
-		}else if(args.length > 0){
-			if(ProxyServer.getInstance().getServerInfo(args[0]) != null) {
+		final ProxiedPlayer player = (ProxiedPlayer) sender;
+		if (args.length == 0) {
+			player.sendMessage(
+					Bungee.Prefix + ChatColor.GRAY + "你現在正處於 " + ChatColor.GOLD + player.getServer().getInfo().getName() + ChatColor.GRAY + " 伺服器");
+			return;
+		} else if (args.length > 0) {
+			if (ProxyServer.getInstance().getServerInfo(args[0]) != null) {
 				player.connect(ProxyServer.getInstance().getServerInfo(args[0]));
-				player.sendMessage(Bungee.Prefix + ChatColor.GRAY + "你從伺服器 " + ChatColor.GOLD + player.getServer().getInfo().getName() + ChatColor.GRAY + " 傳送到了伺服器 " + ChatColor.GOLD + ProxyServer.getInstance().getServerInfo(args[0]).getName());
-			}else {
+				player.sendMessage(Bungee.Prefix + ChatColor.GRAY + "你從伺服器 " + ChatColor.GOLD + player.getServer().getInfo().getName()
+						+ ChatColor.GRAY + " 傳送到了伺服器 " + ChatColor.GOLD + ProxyServer.getInstance().getServerInfo(args[0]).getName());
+			} else {
 				player.sendMessage(Bungee.Prefix + ChatColor.GRAY + "伺服器 " + ChatColor.GOLD + args[0] + ChatColor.GRAY + " 並不存在");
 
 				final Map<String, ServerInfo> ServerList = ProxyServer.getInstance().getServers();
 				final List<String> ServerNameList = new ArrayList<>();
-				for(final Entry<String, ServerInfo> Server : ServerList.entrySet()) {
+				for (final Entry<String, ServerInfo> Server : ServerList.entrySet()) {
 					ServerNameList.add(Server.getValue().getName());
 				}
 
@@ -45,5 +49,3 @@ public class Server extends Command{
 		}
 	}
 }
-
-
