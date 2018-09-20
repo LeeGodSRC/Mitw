89defc8a3adc858e0c95129077ac60ed72fd2544
lulu2002 <lose9749@gmail.com>
2018-09-20 16:45:37 +0800

    再度解決xD

--- Mitw Spigot/src/main/java/net/development/mitw/language/impl/LanguageMessages.java
+++ Mitw Spigot/src/main/java/net/development/mitw/language/impl/LanguageMessages.java
@@ -4,11 +4,11 @@ import org.bukkit.ChatColor;
 
 public class LanguageMessages {
 
-	public String zh_tw_muted = ChatColor.RED + "&c請不要嘗試發送的同樣訊息";
-	public String en_us_muted = ChatColor.RED + "please don't repeat the same message";
+	public String zh_tw_noSpam = ChatColor.RED + "&c請不要嘗試發送的同樣訊息";
+	public String en_us_noSpam = ChatColor.RED + "please don't repeat the same message";
 
-	public String zh_tw_noSpam = ChatColor.RED + "&c請不要說難聽的話,謝謝你";
-	public String en_us_noSpam = ChatColor.RED + "Do Not toxic, thank you";
+	public String zh_tw_muted = ChatColor.RED + "&c請不要說難聽的話,謝謝你";
+	public String en_us_muted = ChatColor.RED + "Do Not toxic, thank you";
 
 	public String zh_tw_inMute = "&c抱歉 你目前處於禁言狀態,你還需要約&f <sec> &c秒後才能說話";
 	public String en_us_inMute = "&cSorry, you are in muted, please wait &f <sec> &cbefore type anything";
