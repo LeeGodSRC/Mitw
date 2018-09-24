22c2d69819215ccb683c8baeaf9df0d531ae392e
lulu2002 <lose9749@gmail.com>
2018-09-24 16:28:26 +0800

    login event change

--- Mitw Spigot/src/main/java/net/development/mitw/listener/PlayerLoginListener.java
+++ Mitw Spigot/src/main/java/net/development/mitw/listener/PlayerLoginListener.java
@@ -2,16 +2,17 @@ package net.development.mitw.listener;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
 
 import net.development.mitw.config.Security;
 
 public class PlayerLoginListener implements Listener {
 	@EventHandler
-	public void onLogin(PlayerLoginEvent e) {
+	public void onLogin(AsyncPlayerPreLoginEvent e) {
 
-		if (!Security.ALLOW_IPS.contains(e.getRealAddress().getHostAddress())) {
-			e.disallow(PlayerLoginEvent.Result.KICK_OTHER, Security.KICK_MESSAGE);
+		if (!Security.ALLOW_IPS.contains(e.getAddress().getHostAddress())) {
+			e.disallow(Result.KICK_WHITELIST, Security.KICK_MESSAGE);
 		}
 	}
 }
