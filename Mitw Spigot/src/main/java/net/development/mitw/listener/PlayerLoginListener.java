260b4d74b4e0ecb11bcf78f69fdfb57ab58aa35c
lulu2002 <lose9749@gmail.com>
2018-09-24 16:45:22 +0800

    remove login listener

--- Mitw Spigot/src/main/java/net/development/mitw/listener/PlayerLoginListener.java
+++ Mitw Spigot/src/main/java/net/development/mitw/listener/PlayerLoginListener.java
@@ -1,18 +1,13 @@
 package net.development.mitw.listener;
 
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
-import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
-
-import net.development.mitw.config.Security;
 
 public class PlayerLoginListener implements Listener {
-	@EventHandler
-	public void onLogin(AsyncPlayerPreLoginEvent e) {
+	/*@EventHandler
+	public void onLogin(PlayerLoginEvent e) {
 
-		if (!Security.ALLOW_IPS.contains(e.getAddress().getHostAddress())) {
-			e.disallow(Result.KICK_WHITELIST, Security.KICK_MESSAGE);
+		if (Security.ALLOW_IPS.contains(e.getRealAddress().getHostAddress())) {
+			e.disallow(PlayerLoginEvent.Result.KICK_OTHER, Security.KICK_MESSAGE);
 		}
-	}
+	}*/
 }
