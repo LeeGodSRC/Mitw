349845ab4f7f9e25d94cd2981b35489c0924794e
lulu2002 <lose9749@gmail.com>
2018-10-06 11:02:54 +0800

    update ItemUtils

--- Mitw Spigot/src/main/java/net/development/mitw/utils/ItemUtil.java
+++ Mitw Spigot/src/main/java/net/development/mitw/utils/ItemUtil.java
@@ -16,206 +16,202 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
 public class ItemUtil {
-	
+
 	@SuppressWarnings("deprecation")
 	public static ItemStack createItem(int id, int amount, int data, String name) {
-        ItemStack item = new ItemStack(id, amount, (byte)data);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.translateAlternateColorCodes((char)'&', (String)name));
-        item.setItemMeta(meta);
-        return item;
-    }
+		final ItemStack item = new ItemStack(id, amount, (byte) data);
+		final ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+		item.setItemMeta(meta);
+		return item;
+	}
 
-    public static ItemStack createSkull(String owner, String name, String ... loreOptions) {
-        ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (byte)3);
-        SkullMeta meta = (SkullMeta)item.getItemMeta();
-        meta.setOwner(owner);
-        meta.setDisplayName(ChatColor.translateAlternateColorCodes((char)'&', (String)name));
-        ArrayList<String> color = new ArrayList<String>();
-        String[] arrstring = loreOptions;
-        int n = arrstring.length;
-        int n2 = 0;
-        while (n2 < n) {
-            String b = arrstring[n2];
-            color.add(ChatColor.translateAlternateColorCodes((char)'&', (String)b));
-            meta.setLore(color);
-            ++n2;
-        }
-        item.setItemMeta((ItemMeta)meta);
-        return item;
-    }
+	public static ItemStack createSkull(String owner, String name, String... loreOptions) {
+		final ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (byte) 3);
+		final SkullMeta meta = (SkullMeta) item.getItemMeta();
+		meta.setOwner(owner);
+		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+		final ArrayList<String> color = new ArrayList<>();
+		final String[] arrstring = loreOptions;
+		final int n = arrstring.length;
+		int n2 = 0;
+		while (n2 < n) {
+			final String b = arrstring[n2];
+			color.add(ChatColor.translateAlternateColorCodes('&', b));
+			meta.setLore(color);
+			++n2;
+		}
+		item.setItemMeta(meta);
+		return item;
+	}
 
-    public static ItemStack createSkull(String owner, String name, Integer amount, String ... loreOptions) {
-        ItemStack item = new ItemStack(Material.SKULL_ITEM, amount, (byte)3);
-        SkullMeta meta = (SkullMeta)item.getItemMeta();
-        meta.setOwner(owner);
-        meta.setDisplayName(ChatColor.translateAlternateColorCodes((char)'&', (String)name));
-        ArrayList<String> color = new ArrayList<String>();
-        String[] arrstring = loreOptions;
-        int n = arrstring.length;
-        int n2 = 0;
-        while (n2 < n) {
-            String b = arrstring[n2];
-            color.add(ChatColor.translateAlternateColorCodes((char)'&', (String)b));
-            meta.setLore(color);
-            ++n2;
-        }
-        item.setItemMeta((ItemMeta)meta);
-        return item;
-    }
-	
+	public static ItemStack createSkull(String owner, String name, Integer amount, String... loreOptions) {
+		final ItemStack item = new ItemStack(Material.SKULL_ITEM, amount, (byte) 3);
+		final SkullMeta meta = (SkullMeta) item.getItemMeta();
+		meta.setOwner(owner);
+		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+		final ArrayList<String> color = new ArrayList<>();
+		final String[] arrstring = loreOptions;
+		final int n = arrstring.length;
+		int n2 = 0;
+		while (n2 < n) {
+			final String b = arrstring[n2];
+			color.add(ChatColor.translateAlternateColorCodes('&', b));
+			meta.setLore(color);
+			++n2;
+		}
+		item.setItemMeta(meta);
+		return item;
+	}
+
 	@SuppressWarnings("deprecation")
-	public static ItemStack createItem1(int id, int amount, int data, String name, String ... lore) {
-        ItemStack item = new ItemStack(id, amount, (short)data);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.translateAlternateColorCodes((char)'&', (String)name));
-        ArrayList<String> color = new ArrayList<String>();
-        String[] arrstring = lore;
-        int n = arrstring.length;
-        int n2 = 0;
-        while (n2 < n) {
-            String b = arrstring[n2];
-            color.add(ChatColor.translateAlternateColorCodes((char)'&', (String)b));
-            meta.setLore(color);
-            ++n2;
-        }
-        item.setItemMeta(meta);
-        return item;
-    }
-    
-    public static ItemStack createItem1(Material mat, int amount, int data, String name, String ... lore) {
-        ItemStack item = new ItemStack(mat, amount, (short)data);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.translateAlternateColorCodes((char)'&', name));
-        ArrayList<String> color = new ArrayList<String>();
-        String[] arrstring = lore;
-        int n = arrstring.length;
-        int n2 = 0;
-        while (n2 < n) {
-            String b = arrstring[n2];
-            color.add(ChatColor.translateAlternateColorCodes('&', b));
-            meta.setLore(color);
-            ++n2;
-        }
-        item.setItemMeta(meta);
-        return item;
-    }
-    
-    
-	
+	public static ItemStack createItem1(int id, int amount, int data, String name, String... lore) {
+		final ItemStack item = new ItemStack(id, amount, (short) data);
+		final ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+		final ArrayList<String> color = new ArrayList<>();
+		final String[] arrstring = lore;
+		final int n = arrstring.length;
+		int n2 = 0;
+		while (n2 < n) {
+			final String b = arrstring[n2];
+			color.add(ChatColor.translateAlternateColorCodes('&', b));
+			meta.setLore(color);
+			++n2;
+		}
+		item.setItemMeta(meta);
+		return item;
+	}
+
+	public static ItemStack createItem1(Material mat, int amount, int data, String name, String... lore) {
+		final ItemStack item = new ItemStack(mat, amount, (short) data);
+		final ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+		final ArrayList<String> color = new ArrayList<>();
+		final String[] arrstring = lore;
+		final int n = arrstring.length;
+		int n2 = 0;
+		while (n2 < n) {
+			final String b = arrstring[n2];
+			color.add(ChatColor.translateAlternateColorCodes('&', b));
+			meta.setLore(color);
+			++n2;
+		}
+		item.setItemMeta(meta);
+		return item;
+	}
+
 	@SuppressWarnings("deprecation")
 	public static ItemStack createItem1(int id, int amount, int data, String name, List<String> lore) {
-        ItemStack item = new ItemStack(id, amount, (short)data);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.translateAlternateColorCodes((char)'&', (String)name));
-        ArrayList<String> color = new ArrayList<String>();
-        for (String b : lore) {
-            color.add(ChatColor.translateAlternateColorCodes((char)'&', (String)b));
-            meta.setLore(color);
-        }
-        item.setItemMeta(meta);
-        return item;
-    }
-    
-    public static ItemStack createItem1(Material mat, int amount, int data, String name, List<String> lore) {
-        ItemStack item = new ItemStack(mat, amount, (short)data);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.translateAlternateColorCodes((char)'&', name));
-        ArrayList<String> color = new ArrayList<String>();
-        for (String b : lore) {
-            color.add(ChatColor.translateAlternateColorCodes((char)'&', (String)b));
-            meta.setLore(color);
-        }
-        item.setItemMeta(meta);
-        return item;
-    }
+		final ItemStack item = new ItemStack(id, amount, (short) data);
+		final ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+		final ArrayList<String> color = new ArrayList<>();
+		for (final String b : lore) {
+			color.add(ChatColor.translateAlternateColorCodes('&', b));
+			meta.setLore(color);
+		}
+		item.setItemMeta(meta);
+		return item;
+	}
 
-    
-	
+	public static ItemStack createItem1(Material mat, int amount, int data, String name, List<String> lore) {
+		final ItemStack item = new ItemStack(mat, amount, (short) data);
+		final ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+		final ArrayList<String> color = new ArrayList<>();
+		for (final String b : lore) {
+			color.add(ChatColor.translateAlternateColorCodes('&', b));
+			meta.setLore(color);
+		}
+		item.setItemMeta(meta);
+		return item;
+	}
+
+	public static ItemStack createEnch(Material mat, int amount, int data, Enchantment ench1, int level1) {
+		final ItemStack item = new ItemStack(mat, amount, (short) data);
+		item.addUnsafeEnchantment(ench1, level1);
+		return item;
+	}
+
 	@SuppressWarnings("deprecation")
-	public static ItemStack createItem1Ench(int id, int amount, int data, String name, String ... lore) {
-        ItemStack item = new ItemStack(id, amount, (short)data);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.translateAlternateColorCodes((char)'&', (String)name));
-        ArrayList<String> color = new ArrayList<String>();
-        String[] arrstring = lore;
-        int n = arrstring.length;
-        int n2 = 0;
-        while (n2 < n) {
-            String b = arrstring[n2];
-            color.add(ChatColor.translateAlternateColorCodes((char)'&', (String)b));
-            meta.setLore(color);
-            ++n2;
-        }
-        item.setItemMeta(meta);
-        EnchantGlow.addGlow(item);
-        return item;
-    }
+	public static ItemStack createItem1Ench(int id, int amount, int data, String name, String... lore) {
+		final ItemStack item = new ItemStack(id, amount, (short) data);
+		final ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+		final ArrayList<String> color = new ArrayList<>();
+		final String[] arrstring = lore;
+		final int n = arrstring.length;
+		int n2 = 0;
+		while (n2 < n) {
+			final String b = arrstring[n2];
+			color.add(ChatColor.translateAlternateColorCodes('&', b));
+			meta.setLore(color);
+			++n2;
+		}
+		item.setItemMeta(meta);
+		EnchantGlow.addGlow(item);
+		return item;
+	}
 
-    
-	
 	@SuppressWarnings("deprecation")
 	public static ItemStack createItem1Ench(int id, int amount, int data, String name, List<String> lore) {
-        ItemStack item = new ItemStack(id, amount, (short)data);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.translateAlternateColorCodes((char)'&', (String)name));
-        ArrayList<String> color = new ArrayList<String>();
-        for (String b : lore) {
-            color.add(ChatColor.translateAlternateColorCodes((char)'&', (String)b));
-            meta.setLore(color);
-        }
-        item.setItemMeta(meta);
-        EnchantGlow.addGlow(item);
-        return item;
-    }
+		final ItemStack item = new ItemStack(id, amount, (short) data);
+		final ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+		final ArrayList<String> color = new ArrayList<>();
+		for (final String b : lore) {
+			color.add(ChatColor.translateAlternateColorCodes('&', b));
+			meta.setLore(color);
+		}
+		item.setItemMeta(meta);
+		EnchantGlow.addGlow(item);
+		return item;
+	}
 
-    
-	
 	@SuppressWarnings("deprecation")
 	public static ItemStack createItem2(int id, int amount, int data, String name, ArrayList<String> lore) {
-        ItemStack item = new ItemStack(id, amount, (short)data);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(name);
-        ArrayList<String> color = new ArrayList<String>();
-        for (String b : lore) {
-            color.add(ChatColor.translateAlternateColorCodes((char)'&', (String)b));
-            meta.setLore(color);
-        }
-        item.setItemMeta(meta);
-        return item;
-    }
+		final ItemStack item = new ItemStack(id, amount, (short) data);
+		final ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(name);
+		final ArrayList<String> color = new ArrayList<>();
+		for (final String b : lore) {
+			color.add(ChatColor.translateAlternateColorCodes('&', b));
+			meta.setLore(color);
+		}
+		item.setItemMeta(meta);
+		return item;
+	}
 
-    
-	
 	@SuppressWarnings("deprecation")
 	public static ItemStack createItem3(int id, int amount, int data, String name, String[] strings) {
-		
-        ItemStack item = new ItemStack(id, amount, (short)data);
-        
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(name);
-        ArrayList<String> color = new ArrayList<String>();
-        
-        for(String string : strings) {
-            color.add(ChatColor.translateAlternateColorCodes((char)'&', (String)string));
-            meta.setLore(color);
-        }
-        
-        item.setItemMeta(meta);
-        return item;
-    }
+
+		final ItemStack item = new ItemStack(id, amount, (short) data);
+
+		final ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(name);
+		final ArrayList<String> color = new ArrayList<>();
+
+		for (final String string : strings) {
+			color.add(ChatColor.translateAlternateColorCodes('&', string));
+			meta.setLore(color);
+		}
+
+		item.setItemMeta(meta);
+		return item;
+	}
 
 	public static ItemStack createItem(Material material, String name) {
-		ItemStack itemStack = new ItemStack(material);
-		ItemMeta meta = itemStack.getItemMeta();
+		final ItemStack itemStack = new ItemStack(material);
+		final ItemMeta meta = itemStack.getItemMeta();
 		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
 		itemStack.setItemMeta(meta);
 		return itemStack;
 	}
-	
+
 	public static ItemStack createItem(Material material, String name, int amount) {
-		ItemStack item = new ItemStack(material, amount);
-		ItemMeta meta = item.getItemMeta();
+		final ItemStack item = new ItemStack(material, amount);
+		final ItemMeta meta = item.getItemMeta();
 
 		meta.setDisplayName(name);
 		item.setItemMeta(meta);
@@ -224,17 +220,17 @@ public class ItemUtil {
 	}
 
 	public static ItemStack createItem(Material material, String name, int amount, short damage) {
-		ItemStack item = new ItemStack(material, amount, damage);
-		ItemMeta meta = item.getItemMeta();
+		final ItemStack item = new ItemStack(material, amount, damage);
+		final ItemMeta meta = item.getItemMeta();
 
 		meta.setDisplayName(name);
 		item.setItemMeta(meta);
 
 		return item;
 	}
-	
+
 	public static ItemStack renameItem(ItemStack item, String name) {
-		ItemMeta meta = item.getItemMeta();
+		final ItemMeta meta = item.getItemMeta();
 
 		meta.setDisplayName(name);
 		item.setItemMeta(meta);
@@ -245,9 +241,9 @@ public class ItemUtil {
 	public static ItemStack reloreItem(ItemStack item, String... lores) {
 		return reloreItem(ReloreType.OVERWRITE, item, lores);
 	}
-	
+
 	public static ItemStack reloreItem(ReloreType type, ItemStack item, String... lores) {
-		ItemMeta meta = item.getItemMeta();
+		final ItemMeta meta = item.getItemMeta();
 
 		List<String> lore = meta.getLore();
 		if (lore == null) {
@@ -255,28 +251,26 @@ public class ItemUtil {
 		}
 
 		switch (type) {
-			case APPEND:
-				lore.addAll(Arrays.asList(lores));
-				meta.setLore(lore);
-				break;
-			case PREPEND:
-				List<String> nLore = new LinkedList<>(Arrays.asList(lores));
-				nLore.addAll(lore);
-				meta.setLore(nLore);
-				break;
-			case OVERWRITE:
-				meta.setLore(Arrays.asList(lores));
-				break;
+		case APPEND:
+			lore.addAll(Arrays.asList(lores));
+			meta.setLore(lore);
+			break;
+		case PREPEND:
+			final List<String> nLore = new LinkedList<>(Arrays.asList(lores));
+			nLore.addAll(lore);
+			meta.setLore(nLore);
+			break;
+		case OVERWRITE:
+			meta.setLore(Arrays.asList(lores));
+			break;
 		}
 
 		item.setItemMeta(meta);
 		return item;
 	}
-	
+
 	public enum ReloreType {
-		OVERWRITE,
-		PREPEND,
-		APPEND
+		OVERWRITE, PREPEND, APPEND
 	}
 }
 
@@ -284,54 +278,57 @@ class EnchantGlow extends EnchantmentWrapper {
 
 	private static Enchantment glow;
 
-    public EnchantGlow(int id) {
-        super(id);
-    }
+	public EnchantGlow(int id) {
+		super(id);
+	}
 
-    public boolean canEnchantItem(ItemStack item) {
-        return true;
-    }
+	@Override
+	public boolean canEnchantItem(ItemStack item) {
+		return true;
+	}
 
-    public boolean conflictsWith(Enchantment other) {
-        return false;
-    }
+	@Override
+	public boolean conflictsWith(Enchantment other) {
+		return false;
+	}
 
-    public EnchantmentTarget getItemTargetInstance() {
-        return null;
-    }
+	public EnchantmentTarget getItemTargetInstance() {
+		return null;
+	}
 
-    public int getMaxLevel() {
-        return 10;
-    }
+	@Override
+	public int getMaxLevel() {
+		return 10;
+	}
 
-    public String getName() {
-        return "Glow";
-    }
+	@Override
+	public String getName() {
+		return "Glow";
+	}
 
-    public int getStartLevel() {
-        return 1;
-    }
+	@Override
+	public int getStartLevel() {
+		return 1;
+	}
 
-    public static Enchantment getGlow() {
-        if (glow != null) {
-            return glow;
-        }
-        try {
-            Field f = Enchantment.class.getDeclaredField("acceptingNew");
-            f.setAccessible(true);
-            f.set(null, true);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        glow = new EnchantGlow(255);
-        Enchantment.registerEnchantment((Enchantment)glow);
-        return glow;
-    }
+	public static Enchantment getGlow() {
+		if (glow != null) {
+			return glow;
+		}
+		try {
+			final Field f = Enchantment.class.getDeclaredField("acceptingNew");
+			f.setAccessible(true);
+			f.set(null, true);
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+		glow = new EnchantGlow(255);
+		Enchantment.registerEnchantment(glow);
+		return glow;
+	}
 
-    public static void addGlow(ItemStack item) {
-        Enchantment glow = EnchantGlow.getGlow();
-        item.addEnchantment(glow, 1);
-    }
+	public static void addGlow(ItemStack item) {
+		final Enchantment glow = EnchantGlow.getGlow();
+		item.addEnchantment(glow, 1);
+	}
 }
-
