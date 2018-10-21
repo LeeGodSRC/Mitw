1c47d0fd19062d2e8d0ebeec867ae7f44aa4124e
BuildTools <unconfigured@null.spigotmc.org>
2018-10-21 09:20:13 +0700

    try it out

--- Mitw Spigot/src/main/java/net/development/mitw/language/SimpleLanguageAPI.java
+++ Mitw Spigot/src/main/java/net/development/mitw/language/SimpleLanguageAPI.java
@@ -1,6 +1,5 @@
 package net.development.mitw.language;
 
-import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -19,7 +18,6 @@ import lombok.Setter;
 import net.development.mitw.Mitw;
 import net.development.mitw.utils.RV;
 import net.development.mitw.utils.StringUtil;
-import net.md_5.bungee.api.ChatColor;
 
 public abstract class SimpleLanguageAPI {
 
@@ -35,8 +33,7 @@ public abstract class SimpleLanguageAPI {
 
 	public SimpleLanguageAPI(final Plugin plugin) {
 		this.plugin = plugin;
-		savedMessages = getMessages();
-		savedMessages.entrySet().forEach(entry -> savedMessages.put(entry.getKey(), entry.getValue().stream().map(s -> StringUtil.cc(s)).collect(Collectors.toList())));
+		getMessages().entrySet().forEach(entry -> savedMessages.put(entry.getKey(), entry.getValue().stream().map(s -> StringUtil.cc(s)).collect(Collectors.toList())));
 	}
 
 	public abstract Map<String, List<String>> getMessages();
@@ -104,31 +101,8 @@ public abstract class SimpleLanguageAPI {
 		if (savedMessages.containsKey(from))
 			return savedMessages.get(from).get(0);
 		else {
-			String to = null;
-			boolean found = false;
-			try {
-				Field field = this.getClass().getDeclaredField(from.replace(".", "_"));
-				Object object = field.get(this.getClass());
-				if (object == null) {
-					field = this.getClass().getClass()
-							.getDeclaredField((LanguageData.DEFAULT_LANGUAGE + "_" + ofrom).replace(".", "_"));
-					field.setAccessible(true);
-					object = field.get(this.getClass());
-				} else {
-					found = true;
-				}
-				to = (String) object;
-			} catch (final Exception e) {
-				Bukkit.getConsoleSender().sendMessage("§cCant get string field " + from.replace(".", "_") + " from "
-						+ this.getClass().getClass().getName() + " from player " + p.getName() + " !");
-				return "null";
-			}
-			if (found) {
-				savedMessages.put(from, Arrays.asList(to));
-			} else {
-				to = ChatColor.translateAlternateColorCodes('&', to);
-			}
-			return to;
+			Bukkit.getConsoleSender().sendMessage("can't find the string from " + from);
+			return "null";
 		}
 	}
 
@@ -137,40 +111,14 @@ public abstract class SimpleLanguageAPI {
 				.collect(Collectors.toList());
 	}
 
-	@SuppressWarnings("unchecked")
 	public List<String> translateArrays(final Player p, final String ofrom) {
 		final String lang = data.getLang(p);
 		final String from = lang + "." + ofrom;
 		if (savedMessages.containsKey(from))
 			return savedMessages.get(from);
 		else {
-			List<String> to = null;
-			boolean found = false;
-			try {
-				Field field = this.getClass().getDeclaredField(from.replace(".", "_"));
-				Object object = field.get(this.getClass());
-				if (object == null) {
-					field = this.getClass().getClass()
-							.getDeclaredField((LanguageData.DEFAULT_LANGUAGE + "_" + ofrom).replace(".", "_"));
-					field.setAccessible(true);
-					object = field.get(this.getClass());
-				} else {
-					found = true;
-				}
-				to = (List<String>) object;
-			} catch (final Exception e) {
-				Bukkit.getConsoleSender().sendMessage("§cCant get string field " + from.replace(".", "_") + " from "
-						+ this.getClass().getClass().getName() + " from player " + p.getName() + " !");
-				return Arrays.asList("null");
-			}
-			if (found) {
-				savedMessages.put(from, to);
-			} else {
-				for (int i = 0; i < to.size(); i++) {
-					to.set(i, ChatColor.translateAlternateColorCodes('&', to.get(i)));
-				}
-			}
-			return to;
+			Bukkit.getConsoleSender().sendMessage("can't find the string from " + from);
+			return Arrays.asList("null");
 		}
 	}
 
